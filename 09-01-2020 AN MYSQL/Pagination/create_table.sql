a052974f1fe991cc6c0d43a7a348ca3cfa88fe5f
Logesh Dinakaran <ddlogesh@gmail.com>
2020-01-16 01:13:21 +0530

    non-practice removed

--- 09-01-2020 AN MYSQL/Pagination/create_table.sql
+++
@@ -1,7 +0,0 @@
-create table emp(
-	id integer primary key,
-    name varchar(30),
-    salary integer
-);
-
-call pagination(3,10);
--- 09-01-2020 AN MYSQL/Pagination/procedure.sql
+++
@@ -1,8 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `pagination`(
-pageno integer,
-count integer
-)
-BEGIN
-	set pageno = (pageno * count);
-	select * from emp limit count offset pageno;
-END
